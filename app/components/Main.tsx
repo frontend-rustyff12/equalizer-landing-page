870b2cb00dd537de822b6236a0ec2e70159cd062
Russell <75209707+rustyff12@users.noreply.github.com>
2025-11-27 00:04:46 +0100

    Footer section

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Card from "./Card";
 export default function Main() {
   return (
-    <main className="relative py-[110px] md:py-[200px] md:px-11 lg:px-40">
+    <main className="relative py-[110px] md:py-[200px] md:px-11 lg:px-40 mb-[520px] md:mb-0">
       <div className="absolute top-0 left-1/2 -translate-x-1/2 w-[210px] h-[430px] md:translate-0 md:w-[270px] md:h-[556px] md:left-[86px] md:top-[86px] lg:w-[350px] lg:h-[642px] lg:top-20 lg:left-[230px]">
         <Image
           src="/illustration-app.png"

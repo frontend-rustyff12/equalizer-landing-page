1181860b21ab27878521002bb1650bc0c958ca93
Russell <75209707+rustyff12@users.noreply.github.com>
2025-11-27 19:17:28 +0100

    UI Fixes. Readme.md update

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,20 +1,16 @@
-import Image from "next/image";
-
 type ButtonProps = {
   text: string;
-  img: string;
+  Icon: React.FC<React.SVGProps<SVGSVGElement>>;
 };
 
-export default function Button({ text, img }: ButtonProps) {
-  const btnStyle =
-    text === "iOS Download"
-      ? "bg-slate-900 text-white"
-      : "bg-white text-slate-900";
+export default function Button({ text, Icon }: ButtonProps) {
+  const btnStyle = text === "iOS Download" ? "btn-1" : "btn-2";
+
   return (
     <button
-      className={`w-full h-16 flex justify-center items-center gap-2  rounded-xl ${btnStyle}`}
+      className={`w-full h-16 flex justify-center items-center gap-2 rounded-xl ${btnStyle}`}
     >
-      <Image src={img} alt="" width={17} height={20} />
+      <Icon className="icon w-[17px] h-5" />
       <span className="mt-1">{text}</span>
     </button>
   );
